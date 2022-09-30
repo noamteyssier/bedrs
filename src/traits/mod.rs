b061a54b7804fd90423f7d632ea22faa14d1868d
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-30 10:43:23 -0700

    create a trait alias for the generic values representing coordinates and the generic intervals

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,5 +1,23 @@
+use std::fmt::Debug;
+
 pub mod container;
 pub mod interval;
-
 pub use container::{Container, Find, Merge};
 pub use interval::{Coordinates, Overlap};
+
+pub trait ValueBounds: 
+where
+    Self: Copy + Default + Ord + Debug 
+{}
+impl <T: Copy + Default + Ord + Debug> ValueBounds for T {}
+
+pub trait IntervalBounds<T>
+where
+    Self: Ord + Coordinates<T> + Clone + Overlap<T> + Copy,
+    T: ValueBounds
+{}
+impl <I, T> IntervalBounds<T> for I
+where
+    I: Ord + Coordinates<T> + Clone + Overlap<T> + Copy,
+    T: ValueBounds,
+{}
