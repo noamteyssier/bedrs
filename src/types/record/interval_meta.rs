4397d0fd0e040409e5723dffcf291f0d87620839
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-29 19:30:39 -0700

    added testing on interval meta

--- src/types/record/interval_meta.rs
+++ src/types/record/interval_meta.rs
@@ -96,5 +96,30 @@ mod testing {
         assert_eq!(interval.start(), start);
         assert_eq!(interval.end(), end);
         assert_eq!(interval.metadata(), &metadata);
+        assert_eq!(interval.chr(), 0);
+    }
+
+    #[test]
+    fn test_interval_meta_functions() {
+        let start = 10;
+        let end = 100;
+        let metadata: Option<usize> = None;
+        let mut interval = IntervalMeta::new(start, end, metadata);
+        interval.update_start(&20);
+        interval.update_end(&30);
+        assert_eq!(interval.start(), 20);
+        assert_eq!(interval.end(), 30);
+    }
+
+    #[test]
+    fn test_interval_meta_from() {
+        let start = 10;
+        let end = 100;
+        let metadata: Option<usize> = None;
+        let a = IntervalMeta::new(start, end, metadata);
+        let b = Coordinates::from(&a);
+        assert_eq!(a.start(), b.start());
+        assert_eq!(a.end(), b.end());
+        assert_eq!(a.chr(), b.chr());
     }
 }
