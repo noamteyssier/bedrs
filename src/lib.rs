c25075bfb668502224683286326d274b0bad07a7
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-29 18:23:17 -0700

    ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 #![warn(clippy::pedantic, clippy::perf)]
-#![allow(clippy::module_inception, clippy::module_name_repetitions, clippy::missing_errors_doc)]
+#![allow(
+    clippy::module_inception,
+    clippy::module_name_repetitions,
+    clippy::missing_errors_doc
+)]
 
 pub mod traits;
 pub mod types;
