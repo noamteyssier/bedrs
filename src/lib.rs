71943e9ebe68808f00d317b45fdbce7a25cf0d55
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-09-29 18:21:22 -0700

    removed nursery lint group

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::pedantic, clippy::perf, clippy::nursery)]
+#![warn(clippy::pedantic, clippy::perf)]
 #![allow(clippy::module_inception, clippy::module_name_repetitions, clippy::missing_errors_doc)]
 
 pub mod traits;
