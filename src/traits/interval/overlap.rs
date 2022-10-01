03cafec7abb6d7526de61ad357f6fb3d6454bdba
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-01 11:49:49 -0700

    added contains and contained_by to overlap

--- src/traits/interval/overlap.rs
+++ src/traits/interval/overlap.rs
@@ -4,6 +4,7 @@ use crate::traits::ValueBounds;
 /// A trait to measure overlaps between intervals implementing `Coordinates`
 pub trait Overlap<T>: Coordinates<T>
 where
+    Self: Sized,
     T: ValueBounds,
 {
     fn bounded_chr<I: Coordinates<T>>(&self, other: &I) -> bool {
@@ -12,8 +13,17 @@ where
     fn interval_overlap<I: Coordinates<T>>(&self, other: &I) -> bool {
         self.start() < other.end() && self.end() > other.start()
     }
+    fn interval_contains<I: Coordinates<T>>(&self, other: &I) -> bool {
+        self.start() < other.start() && self.end() > other.end()
+    }
     fn overlaps<I: Coordinates<T>>(&self, other: &I) -> bool {
         self.bounded_chr(other) && self.interval_overlap(other)
+    }
+    fn contains<I: Coordinates<T>>(&self, other: &I) -> bool {
+        self.bounded_chr(other) && self.interval_contains(other)
+    }
+    fn contained_by<I: Coordinates<T>>(&self, other: &I) -> bool {
+        other.contains(self)
     }
 }
 
@@ -93,5 +103,36 @@ mod testing {
         let a = GenomicInterval::new(1, 10, 20);
         let b = GenomicInterval::new(2, 10, 20);
         assert!(!a.overlaps(&b));
+    }
+
+    #[test]
+    fn test_base_contained() {
+        let a = Interval::new(10, 30);
+        let b = Interval::new(15, 25);
+        let c = Interval::new(10, 30);
+        let d = Interval::new(9, 31);
+        assert!(a.contains(&b));
+        assert!(b.contained_by(&a));
+        assert!(!a.contains(&c));
+        assert!(!a.contained_by(&c));
+        assert!(!a.contains(&d));
+        assert!(a.contained_by(&d));
+    }
+
+    #[test]
+    fn test_genomic_contained() {
+        let a = GenomicInterval::new(1, 10, 30);
+        let b = GenomicInterval::new(1, 15, 25);
+        let c = GenomicInterval::new(1, 10, 30);
+        let d = GenomicInterval::new(1, 9, 31);
+        let e = GenomicInterval::new(2, 15, 25);
+        assert!(a.contains(&b));
+        assert!(b.contained_by(&a));
+        assert!(!a.contains(&c));
+        assert!(!a.contained_by(&c));
+        assert!(!a.contains(&d));
+        assert!(a.contained_by(&d));
+        assert!(!a.contains(&e));
+        assert!(!e.contained_by(&a));
     }
 }
