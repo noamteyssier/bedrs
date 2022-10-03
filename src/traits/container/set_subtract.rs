648c48422b964ac78d35e2d5487862e778e00b6b
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-03 13:47:13 -0700

    added testing for set subtraction from

--- src/traits/container/set_subtract.rs
+++ src/traits/container/set_subtract.rs
@@ -21,7 +21,7 @@ where
         SubtractIter::new(self.records(), query)
     }
 
-    fn subtract_from(&self, query: &I) -> Option<SubtractFromIter<T, I>> {
+    fn subtract_from<'a>(&'a self, query: &'a I) -> Option<SubtractFromIter<T, I>> {
         if self.is_sorted() {
             Some(self.subtract_from_unchecked(query))
         } else {
@@ -29,8 +29,8 @@ where
         }
     }
 
-    fn subtract_from_unchecked(&self, query: &I) -> SubtractFromIter<T, I> {
-        SubtractFromIter::new(self.records(), query)
+    fn subtract_from_unchecked<'a>(&'a self, query: &'a I) -> SubtractFromIter<T, I> {
+        SubtractFromIter::new(self, query)
     }
 }
 
@@ -135,6 +135,104 @@ mod testing {
 
         let iv = subset.next().unwrap();
         assert!(iv.eq(&Interval::new(40, 50)));
+
+        assert!(subset.next().is_none());
+    }
+
+    #[test]
+    /// (q)       x------y
+    /// (a)  i--j
+    /// (b)         k--l
+    /// (c)                m--n        
+    /// ==========================
+    /// (s1)      x-k
+    /// (s2)           l-y
+    fn set_subtract_from_a() {
+        let q = Interval::new(20, 40);
+        let a = Interval::new(10, 15);
+        let b = Interval::new(25, 35);
+        let c = Interval::new(45, 50);
+        let set = IntervalSet::from_sorted(vec![a, b, c]).unwrap();
+        let mut subset = set.subtract_from(&q).unwrap();
+
+        let iv = subset.next().unwrap();
+        println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(20, 25)));
+
+        let iv = subset.next().unwrap();
+        println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(35, 40)));
+
+        assert!(subset.next().is_none());
+    }
+
+    #[test]
+    /// (q)       x------y
+    /// (a)  i------j
+    /// (b)         k--l
+    /// (c)                m--n        
+    /// ==========================
+    /// (s1)           l-y
+    fn set_subtract_from_b() {
+        let q = Interval::new(20, 40);
+        let a = Interval::new(10, 25);
+        let b = Interval::new(25, 35);
+        let c = Interval::new(45, 50);
+        let set = IntervalSet::from_sorted(vec![a, b, c]).unwrap();
+
+        let mut subset = set.subtract_from(&q).unwrap();
+
+        let iv = subset.next().unwrap();
+        println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(35, 40)));
+
+        assert!(subset.next().is_none());
+    }
+
+    #[test]
+    /// (q)       x------y
+    /// (a)  i------j
+    /// (b)         k--l
+    /// (c)            m----n        
+    /// ==========================
+    /// None
+    fn set_subtract_from_c() {
+        let q = Interval::new(20, 40);
+        let a = Interval::new(10, 25);
+        let b = Interval::new(25, 35);
+        let c = Interval::new(35, 50);
+        let set = IntervalSet::from_sorted(vec![a, b, c]).unwrap();
+
+        let mut subset = set.subtract_from(&q).unwrap();
+        assert!(subset.next().is_none());
+    }
+
+    #[test]
+    /// (q)  x----------------y
+    /// (a)     i--j
+    /// (b)            k--l
+    /// ==========================
+    /// (s1) x--i
+    /// (s2)       j---k
+    /// (s3)              l---y
+    fn set_subtract_from_d() {
+        let q = Interval::new(20, 40);
+        let a = Interval::new(25, 27);
+        let b = Interval::new(32, 35);
+        let set = IntervalSet::from_sorted(vec![a, b]).unwrap();
+
+        let mut subset = set.subtract_from(&q).unwrap();
+        let iv = subset.next().unwrap();
+        // println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(20, 25)));
+
+        let iv = subset.next().unwrap();
+        // println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(27, 32)));
+
+        let iv = subset.next().unwrap();
+        // println!("{:?}", iv);
+        assert!(iv.eq(&Interval::new(35, 40)));
 
         assert!(subset.next().is_none());
     }
