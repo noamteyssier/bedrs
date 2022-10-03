f0a191f7bd0f36eac4424d10e23c000c8e08af33
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-03 13:47:29 -0700

    added logic for set subtraction from

--- src/types/iterator/subtract.rs
+++ src/types/iterator/subtract.rs
@@ -1,6 +1,6 @@
 use crate::{
     traits::{IntervalBounds, ValueBounds},
-    Subtract,
+    Subtract, Container, Merge, types::MergeResults,
 };
 use std::marker::PhantomData;
 
@@ -59,33 +59,34 @@ where
 
 }
 
-pub struct SubtractFromIter<'a, T, I>
-where
-    I: IntervalBounds<T> + 'a,
-    T: ValueBounds + 'a,
-{
-    inner: &'a Vec<I>,
-    remainder: I,
-    offset: usize,
-    send_remainder: bool,
-    phantom_t: PhantomData<T>,
-}
-impl<'a, T, I> SubtractFromIter<'a, T, I>
+pub struct SubtractFromIter<T, I>
 where
     I: IntervalBounds<T>,
     T: ValueBounds,
 {
-    pub fn new(inner: &'a Vec<I>, query: &I) -> Self {
+    inner: MergeResults<T, I>,
+    remainder: Option<I>,
+    stack: Option<I>,
+    offset: usize,
+    phantom_t: PhantomData<T>,
+}
+impl<T, I> SubtractFromIter<T, I>
+where
+    I: IntervalBounds<T>,
+    T: ValueBounds,
+{
+    pub fn new<C: Container<T, I>>(container: &C, query: &I) -> Self {
+        let merged_container = container.merge_unchecked();
         Self {
-            inner,
-            remainder: query.to_owned(),
+            inner: merged_container,
+            remainder: Some(query.clone()),
+            stack: None,
             offset: 0,
-            send_remainder: true,
             phantom_t: PhantomData,
         }
     }
 }
-impl<'a, T, I> Iterator for SubtractFromIter<'a, T, I>
+impl<T, I> Iterator for SubtractFromIter<T, I>
 where
     I: IntervalBounds<T>,
     T: ValueBounds,
@@ -93,63 +94,88 @@ where
     type Item = I;
     fn next(&mut self) -> Option<Self::Item> {
         while self.offset < self.inner.len() {
-            // draw the next interval
-            let iv = &self.inner[self.offset];
-            self.offset += 1;
 
-            // equality returns nothing - go to next;
-            if iv.eq(&self.remainder) {
-                continue;
+            // draw the next interval either from the process 
+            // stack or from the internal stockpile 
+            if self.stack.is_none() {
+                let iv = &self.inner.records()[self.offset];
+                self.offset += 1;
+                self.stack = Some(iv.clone());
             }
-
-            // case where there is no overlap
-            if !iv.overlaps(&self.remainder) {
-
-                // if the interval is right shifted
-                // make sure to store the current interval
-                // and send any remainder
-                if iv.gt(&self.remainder) {
-                    if self.send_remainder {
-                        let some_iv = self.remainder.to_owned();
-                        self.remainder = iv.to_owned();
-                        return Some(some_iv);
-                    } 
-                }
-                return Some(iv.to_owned())
-            }
-
-            // perform the subtraction
-            let mut sub_intervals = self.remainder.subtract(iv).expect("in subtraction set");
-
-            // case where interval is interally overlapped
-            let return_iv = if sub_intervals.len().eq(&2) {
-                self.remainder = sub_intervals.pop().unwrap();
-                sub_intervals.pop()
-            } else {
-                // pop the interval for inspection
-                let some_iv = sub_intervals.pop().unwrap();
-
-                // case where interval is left-shifted to query
-                if some_iv.gt(&self.remainder) {
-                    self.remainder.update_start(&some_iv.start());
-                    continue;
-
-                // case where interval is right-shifted to query
-                } else {
-                    self.send_remainder = false;
-                    Some(some_iv)
-                }
+            let iv = match &self.stack {
+                Some(iv) => iv,
+                None => {break;}
             };
 
-            return return_iv;
+            // println!("====");
+            // println!("Interval -> {:?} {:?}", iv.start(), iv.end());
+
+            // Perform subtraction logic if there is a remainder left
+            if let Some(ref mut remainder) = self.remainder {
+                // println!("Remainder -> {:?} {:?}", remainder.start(), remainder.end());
+
+                // Case where the interval does not overlap with the remainder
+                // and it is right-shifted
+                //
+                // this returns the remainder and keeps the interval in the stack
+                if !iv.overlaps(remainder) && iv.gt(remainder) {
+                    let tmp = remainder.clone();
+                    self.remainder = None;
+                    return Some(tmp);
+                }
+
+                // If the remainder is contained by the interval then it is
+                // fully squashed and no further intervals are necessary
+                if iv.contains(remainder) {
+                    self.remainder = None;
+                    return None;
+                }
+
+                // Performs the subtraction
+                let mut sub = iv.subtract(remainder).unwrap();
+
+                // Case where there is a left or right shift in the subtraction
+                if sub.len() == 1 {
+                    let siv = sub.pop().unwrap();
+
+                    // If the interval is left-shifted the remainder must be updated
+                    // to reflect the endpoints of the cut interval
+                    if siv.lt(remainder) {
+                        if siv.end().ge(&remainder.start()) {
+                            remainder.update_start(&iv.end()); 
+                        } else {
+                            self.stack = None;
+                            continue;
+                        }
+                    // Otherwise we can just return the right-shifted cut interval
+                    } else {
+                        return Some(siv)
+                    }
+                // If the interval is contained by the interval then there are two cut
+                // sub intervals.
+                //
+                // The RHS interval becomes the new remainder, and the LHS interval is returned
+                } else {
+                    self.remainder = sub.pop();
+                    let result_siv = sub.pop();
+                    self.stack = None;
+                    return result_siv;
+                }
+
+                // Clears the stack at the end of all logical choices
+                self.stack = None;
+            }
         }
 
-        // sends any remaining remainder
-        if self.send_remainder {
-            self.send_remainder = false;
-            Some(self.remainder.to_owned())
-        } else {
-            None
+        // Case where there's still a remainder and the iterator
+        // is past all the set intervals
+        if let Some(ref remainder) = self.remainder {
+            let tmp = remainder.clone();
+            self.remainder = None;
+            return Some(tmp)
         }
+
+        // terminal case
+        None
     }
 }
