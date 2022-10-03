582cf4cb27f9ac773171998bee920b621eb35e96
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-03 10:24:19 -0700

    made stitchiter public on the types level

--- src/types/iterator/mod.rs
+++ src/types/iterator/mod.rs
@@ -1,4 +1,7 @@
 mod find;
 mod subtract;
+mod stitch;
 pub use find::{FindIter, FindIterSorted};
 pub use subtract::SubtractIter;
+pub use stitch::StitchIter;
+
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,5 +2,5 @@ pub mod container;
 pub mod iterator;
 pub mod record;
 pub use container::{GenomicIntervalSet, IntervalMetaSet, IntervalSet, MergeResults};
-pub use iterator::{FindIter, FindIterSorted, SubtractIter};
+pub use iterator::{FindIter, FindIterSorted, SubtractIter, StitchIter};
 pub use record::{GenomicInterval, Interval, IntervalMeta};
