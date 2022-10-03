415fd7abb9fac9286d22bd1f3db551e21213fed8
noamteyssier <22600644+noamteyssier@users.noreply.github.com>
2022-10-02 22:25:34 -0700

    made subtractiter public at the types level

--- src/types/iterator/mod.rs
+++ src/types/iterator/mod.rs
@@ -1,2 +1,4 @@
 mod find;
+mod subtract;
 pub use find::{FindIter, FindIterSorted};
+pub use subtract::SubtractIter;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,5 +2,5 @@ pub mod container;
 pub mod iterator;
 pub mod record;
 pub use container::{GenomicIntervalSet, IntervalMetaSet, IntervalSet, MergeResults};
-pub use iterator::{FindIter, FindIterSorted};
+pub use iterator::{FindIter, FindIterSorted, SubtractIter};
 pub use record::{GenomicInterval, Interval, IntervalMeta};
