c509ff3a409d2536622c8f2ccf8c8454a6d1e761
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-09 08:58:01 -0700

    added enum to select type of query method in find

--- src/types/iterator/find.rs
+++ src/types/iterator/find.rs
@@ -1,6 +1,13 @@
 use crate::traits::{IntervalBounds, ValueBounds};
 use std::marker::PhantomData;
 
+pub enum QueryMethod<T: ValueBounds> 
+{
+    Compare,
+    CompareBy(T),
+    CompareExact(T),
+}
+
 pub struct FindIter<'a, T, I>
 where
     I: IntervalBounds<T> + 'a,
@@ -10,18 +17,27 @@ where
     query: &'a I,
     offset: usize,
     phantom_t: PhantomData<T>,
+    method: QueryMethod<T>,
 }
 impl<'a, T, I> FindIter<'a, T, I>
 where
     I: IntervalBounds<T>,
     T: ValueBounds,
 {
-    pub fn new(inner: &'a Vec<I>, query: &'a I) -> Self {
+    pub fn new(inner: &'a Vec<I>, query: &'a I, method: QueryMethod<T>) -> Self {
         Self {
             inner,
             query,
             offset: 0,
             phantom_t: PhantomData,
+            method,
+        }
+    }
+    fn predicate(&self, interval: &I) -> bool {
+        match self.method {
+            QueryMethod::Compare => interval.overlaps(self.query),
+            QueryMethod::CompareBy(val) => interval.overlaps_by(self.query, val),
+            QueryMethod::CompareExact(val) => interval.overlaps_by_exactly(self.query, val),
         }
     }
 }
@@ -35,7 +51,7 @@ where
         while self.offset < self.inner.len() {
             let interval = &self.inner[self.offset];
             self.offset += 1;
-            if interval.overlaps(self.query) {
+            if self.predicate(interval) {
                 return Some(interval);
             }
         }
@@ -52,18 +68,27 @@ where
     query: &'a I,
     offset: usize,
     phantom_t: PhantomData<T>,
+    method: QueryMethod<T>,
 }
 impl<'a, T, I> FindIterSorted<'a, T, I>
 where
     I: IntervalBounds<T>,
     T: ValueBounds,
 {
-    pub fn new(inner: &'a Vec<I>, query: &'a I, offset: usize) -> Self {
+    pub fn new(inner: &'a Vec<I>, query: &'a I, offset: usize, method: QueryMethod<T>) -> Self {
         Self {
             inner,
             query,
             offset,
             phantom_t: PhantomData,
+            method,
+        }
+    }
+    fn predicate(&self, interval: &I) -> bool {
+        match self.method {
+            QueryMethod::Compare => interval.overlaps(self.query),
+            QueryMethod::CompareBy(val) => interval.overlaps_by(self.query, val),
+            QueryMethod::CompareExact(val) => interval.overlaps_by_exactly(self.query, val),
         }
     }
 }
@@ -77,7 +102,7 @@ where
         while self.offset < self.inner.len() {
             let interval = &self.inner[self.offset];
             self.offset += 1;
-            if interval.overlaps(self.query) {
+            if self.predicate(interval) {
                 return Some(interval);
             } else if interval.start() >= self.query.end() {
                 break;
--- src/types/iterator/mod.rs
+++ src/types/iterator/mod.rs
@@ -1,4 +1,4 @@
 mod find;
 mod subtract;
-pub use find::{FindIter, FindIterSorted};
+pub use find::{FindIter, FindIterSorted, QueryMethod};
 pub use subtract::{SubtractFromIter, SubtractIter};
