592072f2493c3c181ed47df8fbc36d491fba078d
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-12 11:57:28 -0700

    make intersect iter public

--- src/types/iterator/mod.rs
+++ src/types/iterator/mod.rs
@@ -1,6 +1,8 @@
 mod find;
+mod intersect;
 mod merge;
 mod subtract;
 pub use find::{f_len, FindIter, FindIterSorted, QueryMethod};
+pub use intersect::IntersectIter;
 pub use merge::MergeIter;
 pub use subtract::{SubtractFromIter, SubtractIter};
