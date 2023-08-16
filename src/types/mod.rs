e96625f82711efc1b698a4f6635d408b4be318fd
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-16 13:01:00 -0700

    remove intervalmeta and its set - not very useful as a built-in type

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,12 +1,8 @@
 pub mod container;
 pub mod iterator;
 pub mod record;
-pub use container::{
-    GenomicIntervalSet, IntervalMetaSet, IntervalSet, MergeResults, StrandedGenomicIntervalSet,
-};
+pub use container::{GenomicIntervalSet, IntervalSet, MergeResults, StrandedGenomicIntervalSet};
 pub use iterator::{
     FindIter, FindIterSorted, IntersectIter, MergeIter, SubtractFromIter, SubtractIter,
 };
-pub use record::{
-    GenomicInterval, Interval, IntervalMeta, NamedInterval, Strand, StrandedGenomicInterval,
-};
+pub use record::{GenomicInterval, Interval, NamedInterval, Strand, StrandedGenomicInterval};
--- src/types/record/mod.rs
+++ src/types/record/mod.rs
@@ -1,10 +1,8 @@
 mod genomic_interval;
 mod interval;
-mod interval_meta;
 mod named_interval;
 mod stranded_genomic_interval;
 pub use genomic_interval::GenomicInterval;
 pub use interval::Interval;
-pub use interval_meta::IntervalMeta;
 pub use named_interval::NamedInterval;
 pub use stranded_genomic_interval::{Strand, StrandedGenomicInterval};
