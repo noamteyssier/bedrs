87eb05eb77bfddb2774ed9a373666d6a7c23adfb
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-08-17 15:02:38 -0700

    added closest downstream and fix both to use T::max() instead of the first interval to avoid double calling the first

--- src/traits/container/closest.rs
+++ src/traits/container/closest.rs
@@ -32,6 +32,17 @@ where
         }
     }
 
+    fn closest_downstream(&self, query: &I) -> Result<Option<&I>, SetError> {
+        if self.is_sorted() {
+            if self.records().is_empty() {
+                return Err(SetError::EmptySet);
+            }
+            Ok(self.closest_downstream_unchecked(query))
+        } else {
+            Err(SetError::UnsortedSet)
+        }
+    }
+
     fn closest_unchecked(&self, query: &I) -> Option<&I> {
         if let Some(bound) = self.chr_bound_unchecked(query) {
             let mut current_dist = T::max_value();
@@ -59,9 +70,7 @@ where
 
     fn closest_upstream_unchecked(&self, query: &I) -> Option<&I> {
         if let Some(bound) = self.chr_bound_upstream_unchecked(query) {
-            let start_iv = &self.records()[bound];
-            let mut current_dist = query.distance(start_iv)?;
-
+            let mut current_dist = T::max_value();
             let mut current_lowest = bound;
             let mut position = bound;
             loop {
@@ -70,6 +79,34 @@ where
                 }
                 let test_iv = &self.records()[position];
                 if test_iv.gt(query) {
+                    break;
+                }
+                let distance = query.distance(test_iv)?;
+                if distance < current_dist {
+                    current_dist = distance;
+                    current_lowest = position;
+                } else if distance > current_dist || distance == current_dist {
+                    break;
+                }
+                position += 1;
+            }
+            Some(&self.records()[current_lowest])
+        } else {
+            None
+        }
+    }
+
+    fn closest_downstream_unchecked(&self, query: &I) -> Option<&I> {
+        if let Some(bound) = self.chr_bound_downstream_unchecked(query) {
+            let mut current_dist = T::max_value();
+            let mut current_lowest = bound;
+            let mut position = bound;
+            loop {
+                if position == self.len() {
+                    break;
+                }
+                let test_iv = &self.records()[position];
+                if test_iv.lt(query) {
                     break;
                 }
                 let distance = query.distance(test_iv)?;
@@ -103,6 +140,8 @@ mod testing {
         let query = GenomicInterval::new(1, 22, 23);
         let set = GenomicIntervalSet::new(intervals);
         assert!(set.closest(&query).is_err());
+        assert!(set.closest_upstream(&query).is_err());
+        assert!(set.closest_downstream(&query).is_err());
     }
 
     #[test]
@@ -111,6 +150,8 @@ mod testing {
         let query = GenomicInterval::new(1, 22, 23);
         let set = GenomicIntervalSet::from_unsorted(intervals);
         assert!(set.closest(&query).is_err());
+        assert!(set.closest_upstream(&query).is_err());
+        assert!(set.closest_downstream(&query).is_err());
     }
 
     #[test]
@@ -245,5 +286,75 @@ mod testing {
         let set = GenomicIntervalSet::from_unsorted(intervals);
         let closest = set.closest_upstream(&query).unwrap().unwrap();
         assert_eq!(closest, &GenomicInterval::new(2, 30, 40));
+    }
+
+    #[test]
+    /// |1|  x-----y  |2| x---y  x-----y   x-------y
+    /// |2|                          i-------j
+    /// =====================================
+    ///                          x-----y
+    fn closest_upstream_c() {
+        let intervals = vec![
+            GenomicInterval::new(1, 10, 20),
+            GenomicInterval::new(2, 10, 20),
+            GenomicInterval::new(2, 30, 40),
+            GenomicInterval::new(2, 50, 60),
+        ];
+        let query = GenomicInterval::new(2, 32, 55);
+        let set = GenomicIntervalSet::from_unsorted(intervals);
+        let closest = set.closest_upstream(&query).unwrap().unwrap();
+        assert_eq!(closest, &GenomicInterval::new(2, 30, 40));
+    }
+
+    #[test]
+    ///    x-----y      x-----y   x-------y
+    ///           i-------j
+    /// =====================================
+    ///                 x-----y
+    fn closest_downstream_a() {
+        let intervals = vec![
+            GenomicInterval::new(1, 10, 20),
+            GenomicInterval::new(1, 30, 40),
+            GenomicInterval::new(1, 50, 60),
+        ];
+        let query = GenomicInterval::new(1, 22, 32);
+        let set = GenomicIntervalSet::from_unsorted(intervals);
+        let closest = set.closest_downstream(&query).unwrap().unwrap();
+        assert_eq!(closest, &GenomicInterval::new(1, 30, 40));
+    }
+
+    #[test]
+    /// |1|  x-----y  |2|  x-----y   x-------y
+    /// |2|                   i-------j
+    /// =====================================
+    ///                              x-------y
+    fn closest_downstream_b() {
+        let intervals = vec![
+            GenomicInterval::new(1, 10, 20),
+            GenomicInterval::new(2, 30, 40),
+            GenomicInterval::new(2, 50, 60),
+        ];
+        let query = GenomicInterval::new(2, 32, 55);
+        let set = GenomicIntervalSet::from_unsorted(intervals);
+        let closest = set.closest_downstream(&query).unwrap().unwrap();
+        assert_eq!(closest, &GenomicInterval::new(2, 50, 60));
+    }
+
+    #[test]
+    /// |1|  x-----y  |2|  x-----y   x-------y  x-----y
+    /// |2|                   i-------j
+    /// =====================================
+    ///                              x-------y
+    fn closest_downstream_c() {
+        let intervals = vec![
+            GenomicInterval::new(1, 10, 20),
+            GenomicInterval::new(2, 30, 40),
+            GenomicInterval::new(2, 50, 60),
+            GenomicInterval::new(2, 70, 80),
+        ];
+        let query = GenomicInterval::new(2, 32, 55);
+        let set = GenomicIntervalSet::from_unsorted(intervals);
+        let closest = set.closest_downstream(&query).unwrap().unwrap();
+        assert_eq!(closest, &GenomicInterval::new(2, 50, 60));
     }
 }
