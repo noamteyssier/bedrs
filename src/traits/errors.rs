ad78e8abebb58fec12624ad29c096aa318931562
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2024-04-02 16:18:15 -0700

    tests: added coverage over set errors

--- src/traits/errors.rs
+++ src/traits/errors.rs
@@ -21,3 +21,36 @@ pub enum SetError {
     #[error("Provided fraction {frac} is oversized. Must be (0, 1]")]
     FractionUnbounded { frac: f64 },
 }
+
+#[cfg(test)]
+mod testing {
+    use super::*;
+
+    #[test]
+    fn test_set_error() {
+        let err = SetError::UnsortedSet;
+        assert_eq!(format!("{err}"), "The interval set is unsorted.");
+        let err = SetError::EmptySet;
+        assert_eq!(format!("{err}"), "The interval set is empty.");
+        let err = SetError::UnsortedIntervals;
+        assert_eq!(format!("{err}"), "The provided intervals are not sorted.");
+        let err = SetError::MissingMaxLen;
+        assert_eq!(format!("{err}"), "The maximum interval length is unknown");
+        let err = SetError::SampleSizeTooLarge;
+        assert_eq!(
+            format!("{err}"),
+            "Sample size is larger than the number of intervals."
+        );
+        let err = SetError::FractionUnbounded { frac: 1.0 };
+        assert_eq!(
+            format!("{err}"),
+            "Provided fraction 1 is oversized. Must be (0, 1]"
+        );
+    }
+
+    #[test]
+    fn test_set_error_debug() {
+        let err = SetError::UnsortedSet;
+        assert_eq!(format!("{err:?}"), "UnsortedSet");
+    }
+}
